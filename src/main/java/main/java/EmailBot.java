431c8d0772c579bf45220f5debd644b52b4e9b75
SirBeans <ryanchung0084@gmail.com>
2026-05-19 22:31:56 -0400

    inline files

--- src/main/java/main/java/EmailBot.java
+++ src/main/java/main/java/EmailBot.java
@@ -6,7 +6,6 @@ import com.opencsv.exceptions.CsvException;
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
-import javax.mail.internet.InternetAddress;
 import javax.mail.*;
 import javax.mail.internet.*;
 
@@ -17,21 +16,28 @@ import java.util.*;
 
 public class EmailBot {
 
-    private static final String USERNAME = ""; // Replace with your email
-    private static final String PASSWORD = "";    // Use an App Password
-    private static final String SUBJECT = "Robotics Summer Camp Opportunity";
-    private static final String SENDER_NAME = "Ryan Chung";     //Add your name
+    private static final String USERNAME    = "rchung@team4099.com";
+    private static final String PASSWORD    = "tsvf gknm opum emcu";
+    private static final String SUBJECT     = "Robotics Summer Camp Opportunity";
+    private static final String SENDER_NAME = "Ryan Chung";
     private static final String TIME_OF_DAY = "afternoon";
-    private static final List<String> ATTACHMENTS = Arrays.asList("Robocamps Flyer 2025.png");
+
+    private static final List<String> INLINE_ATTACHMENTS = Arrays.asList(
+            "Robocamps_Flyer_2025.png"
+    );
+
+    private static final List<String> FILE_ATTACHMENTS = Arrays.asList(
+            "Robocamps_Flyer_2025.png"
+    );
 
     public static void main(String[] args) {
         List<String[]> schools = readSchoolsFromCSV("schools.csv");
 
         for (String[] school : schools) {
             String schoolName = school[0];
-            String email = school[1];
-            String messageBody = getEmailBody(schoolName, SENDER_NAME, TIME_OF_DAY);
-            sendEmail(email, SUBJECT, messageBody, ATTACHMENTS);
+            String email      = school[1];
+            String htmlBody   = getEmailBody(schoolName, SENDER_NAME, TIME_OF_DAY);
+            sendEmail(email, SUBJECT, htmlBody, INLINE_ATTACHMENTS, FILE_ATTACHMENTS);
         }
     }
 
@@ -49,39 +55,66 @@ public class EmailBot {
     }
 
     public static String getEmailBody(String schoolName, String senderName, String timeOfDay) {
-        return "Good " + timeOfDay + ",\n\n" +
-                "My name is " + senderName + " and I’m a member of FIRST Robotics Team 4099, " +
-                "a student-led team based in Poolesville, MD. We’re extremely excited to introduce our RoboCamps summer program! " +
-                "Every summer, we hold this week-long camp at Poolesville Self Defense to help students ages 8–14 explore computer science " +
-                "and engineering through hands-on instruction.\n\n" +
-                "Over the course of a week, campers work in small groups to design, build, and program their own robot using materials " +
-                "from the VEX IQ Ecosystem. This culminates in a final competition at the end where campers can show off their robots and earn special prizes!\n\n" +
-                "We’re reaching out to you because we wanted to know if you would be willing to help us advertise our program to your students at " + schoolName + ". " +
-                "More information about the opportunity can be found on the RoboCamps page of our website, and I’ve attached an announcement blurb and our flyer below. " +
-                "In the past, schools have sent announcements or parent emails, but we are open to anything!\n\n" +
-                "If you have any questions or concerns, feel free to reply to this email or contact the team at robocamps@team4099.com.\n\n" +
-                "Thank you so much for your time and consideration!\n\n" +
-                senderName + "\n\n" +
-                "Sample Announcement:\n" +
-                "FIRST Robotics Team 4099 is hosting RoboCamps this year! RoboCamps is a week-long summer camp held at Poolesville Self Defense " +
-                "where students, ages 8–14, learn how to design, build, and code their own robot. This interactive camp culminates in a final competition " +
-                "on the last day, where campers can show off their robots and compete for a special prize! Click here to take a look at our program’s website. " +
-                "If you or someone you know might be interested in this program, registration is open! If you have any questions, feel free to email robocamps@team4099.com. " +
-                "We hope to see you there!";
+        return "<!DOCTYPE html><html><body style='font-family:Arial,sans-serif;font-size:14px;'>"
+
+                + "<p>Good " + timeOfDay + ",</p>"
+
+                + "<p>My name is <strong>" + senderName + "</strong> and I'm a member of FIRST Robotics Team 4099, "
+                + "a student-led team based in Poolesville, MD. We're extremely excited to introduce our "
+                + "<strong>RoboCamps</strong> summer program! "
+                + "Every summer, we hold this week-long camp at Poolesville Self Defense to help students ages 8–14 "
+                + "explore computer science and engineering through hands-on instruction.</p>"
+
+                + "<p>Over the course of a week, campers work in small groups to design, build, and program their own "
+                + "robot using materials from the VEX IQ Ecosystem. This culminates in a final competition at the end "
+                + "where campers can show off their robots and earn special prizes!</p>"
+
+                + "<p>We're reaching out to you because we wanted to know if you would be willing to help us advertise "
+                + "our program to your students at <strong>" + schoolName + "</strong>. "
+                + "More information can be found on the RoboCamps page of our website. "
+                + "In the past, schools have sent announcements or parent emails, but we are open to anything!</p>"
+
+                + "<p><img src='cid:Robocamps_Flyer_2025.png' "
+                + "     alt='RoboCamps Flyer' style='max-width:600px;width:100%;'/></p>"
+
+                + "<p>If you have any questions or concerns, feel free to reply to this email or contact the team at "
+                + "<a href='mailto:robocamps@team4099.com'>robocamps@team4099.com</a>.</p>"
+
+                + "<p>Thank you so much for your time and consideration!</p>"
+
+                + "<p>" + senderName + "</p>"
+
+                + "<hr/>"
+                + "<p><strong>Sample Announcement:</strong><br/>"
+                + "FIRST Robotics Team 4099 is hosting RoboCamps this year! RoboCamps is a week-long summer camp held "
+                + "at Poolesville Self Defense where students, ages 8–14, learn how to design, build, and code their "
+                + "own robot. This interactive camp culminates in a final competition on the last day, where campers "
+                + "can show off their robots and compete for a special prize! "
+                + "<a href='https://team4099.com/robocamps'>Click here</a> to take a look at our program's website. "
+                + "If you or someone you know might be interested, registration is open! "
+                + "Email <a href='mailto:robocamps@team4099.com'>robocamps@team4099.com</a> with any questions. "
+                + "We hope to see you there!</p>"
+
+                + "</body></html>";
     }
 
-    public static void sendEmail(String to, String subject, String body, List<String> filePaths) {
+    public static void sendEmail(String to,
+                                 String subject,
+                                 String htmlBody,
+                                 List<String> inlineFiles,
+                                 List<String> attachedFiles) {
+
         final String CC_EMAIL = "robocamps@team4099.com";
 
         Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.auth",            "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.host",            "smtp.gmail.com");
+        props.put("mail.smtp.port",            "587");
 
         Session session = Session.getInstance(props, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new javax.mail.PasswordAuthentication(USERNAME, PASSWORD);
+                return new PasswordAuthentication(USERNAME, PASSWORD);
             }
         });
 
@@ -92,23 +125,39 @@ public class EmailBot {
             message.setRecipients(Message.RecipientType.CC, InternetAddress.parse(CC_EMAIL));
             message.setSubject(subject);
 
-            Multipart multipart = new MimeMultipart();
+            MimeBodyPart htmlPart = new MimeBodyPart();
+            htmlPart.setContent(htmlBody, "text/html; charset=utf-8");
 
-            // Email body
-            MimeBodyPart textPart = new MimeBodyPart();
-            textPart.setText(body);
-            multipart.addBodyPart(textPart);
+            MimeMultipart relatedPart = new MimeMultipart("related");
+            relatedPart.addBodyPart(htmlPart);
 
-            // Attachments
-            for (String filePath : filePaths) {
-                MimeBodyPart attachment = new MimeBodyPart();
-                DataSource source = new FileDataSource(filePath);
-                attachment.setDataHandler(new DataHandler(source));
-                attachment.setFileName(new File(filePath).getName());
-                multipart.addBodyPart(attachment);
+            for (String filePath : inlineFiles) {
+                MimeBodyPart inlinePart = new MimeBodyPart();
+                DataSource ds = new FileDataSource(filePath);
+                inlinePart.setDataHandler(new DataHandler(ds));
+                String fileName = new File(filePath).getName();
+                inlinePart.setHeader("Content-ID", "<" + fileName + ">");
+                inlinePart.setDisposition(MimeBodyPart.INLINE);
+                inlinePart.setFileName(fileName);
+                relatedPart.addBodyPart(inlinePart);
             }
 
-            message.setContent(multipart);
+            MimeBodyPart relatedWrapper = new MimeBodyPart();
+            relatedWrapper.setContent(relatedPart);
+
+            MimeMultipart mixedPart = new MimeMultipart("mixed");
+            mixedPart.addBodyPart(relatedWrapper);
+
+            for (String filePath : attachedFiles) {
+                MimeBodyPart attachPart = new MimeBodyPart();
+                DataSource ds = new FileDataSource(filePath);
+                attachPart.setDataHandler(new DataHandler(ds));
+                attachPart.setFileName(new File(filePath).getName());
+                attachPart.setDisposition(MimeBodyPart.ATTACHMENT);
+                mixedPart.addBodyPart(attachPart);
+            }
+
+            message.setContent(mixedPart);
             Transport.send(message);
 
             System.out.println("Email sent to: " + to);
@@ -117,4 +166,3 @@ public class EmailBot {
         }
     }
 }
-
